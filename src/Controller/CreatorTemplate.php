3561c6cab96a8fdb69135101bcce7ca20b45c36e
arslan <arslanafzal321@gmail.com>
2019-03-02 02:39:44 +0500

    Creator template created. NoteFactory created.

--- src/Controller/CreatorTemplate.php
+++ src/Controller/CreatorTemplate.php
@@ -3,13 +3,34 @@
 namespace EresNote\Controller;
 
 use EresNote\Domain\Entity\AbstractEntity;
-use EresNote\Domain\Service\Http\RequestAdapter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
 abstract class CreatorTemplate
 {
-    public function create(RequestAdapter $requestAdapter)
+    public function create(Request $request, ValidatorInterface $validator)
+    {
+        $factory = $this->getFactory();
+        $entity = $factory->createFromParameters($request->get()->all());
+
+        $errors = $validator->validate($entity);
+        if ($errors->count() === 0) {
+            $this->processPreSuccessHook();
+            return $this->getSuccessResponse();
+        }
+
+        return $this->getFailureResponse();
+    }
+
+    abstract protected function getFactory();
+
+    protected function processPreSuccessHook()
     {
 
     }
+
+    abstract protected function getSuccessResponse();
+
+    abstract protected function getFailureResponse();
 }
--- src/Controller/NoteCreatorController.php
+++ src/Controller/NoteCreatorController.php
@@ -10,26 +10,27 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
-class NoteCreatorController
+class NoteCreatorController extends CreatorTemplate
 {
-    public function create(Request $request, ValidatorInterface $validator, NoteRepositoryInterface $noteRepository)
-    {
-        $note = new Note();
 
-        $note->title = $request->get('title', "");
-        $note->content = $request->get('content', "");
-        $note->creationTimestamp = $request->get('creationTimestamp', "");
-        $note->user = $request->get('user', 1);
-
-        $errors = $validator->validate($note);
-
-        if ($errors->count() == 0) {
-            $noteRepository->persist($note);
-        }
-
-        return new Response("Done");
-
-    }
+//    public function create(Request $request, ValidatorInterface $validator, NoteRepositoryInterface $noteRepository)
+//    {
+//        $note = new Note();
+//
+//        $note->title = $request->get('title', "");
+//        $note->content = $request->get('content', "");
+//        $note->creationTimestamp = $request->get('creationTimestamp', "");
+//        $note->user = $request->get('user', 1);
+//
+//        $errors = $validator->validate($note);
+//
+//        if ($errors->count() == 0) {
+//            $noteRepository->persist($note);
+//        }
+//
+//        return new Response("Done");
+//
+//    }
 //    /**
 //     *  @ParamConverter("note", options={"mapping": {"title": "title", "content": "content", "id":"id"}})
 //     */
